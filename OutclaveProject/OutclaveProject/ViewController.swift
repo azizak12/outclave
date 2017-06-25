08c6376b1d19efa781892f48362e165f73d77f3b
azizak12 <azizak712522@gmail.com>
2017-06-25 09:37:38 -0400

    Add files via upload

--- OutclaveProject/OutclaveProject/ViewController.swift
+++ OutclaveProject/OutclaveProject/ViewController.swift
@@ -8,56 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
     
     
-    
+    @IBOutlet weak var label: UILabel!
     @IBOutlet weak var picker: UIPickerView!
     
-    var pickerData: [String] = [String]()
+    let languages = ["English", "Español", "普通话", "हिंदी", "Français", "Italiano", "Deutsche", "日本語",  "فارسی", "Pусский"]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // Connect data:
-        self.picker.delegate = self
-        self.picker.dataSource = self
-        
-        // Input data into the Array:
-        pickerData = ["English", "Español", "普通话", "हिंदी", "Français", "Italiano", "Deutsche", "日本語",  "فارسی", "Pусский"]
-        
-        
-    }
-    
-    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-        let titleData = pickerData[row]
-        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "Avenir", size: 15.0)!,NSForegroundColorAttributeName:UIColor.whiteColor()])
-        return myTitle
-    }
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    // The number of columns of data
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    func numberOfComponents(in pickerView: UIPickerView) ->  Int
+    {
         return 1
     }
     
-    // The number of rows of data
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return languages[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return languages.count
     }
     
-    // The data to return for the row and component (column) that's being passed in
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row]
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        label.text = languages[row]
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
-}
 
+}
